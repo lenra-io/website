5f910bff8cf51d21f6b017d94a0e2246bcef85f1
thomas_lenra <thomas.darocha@lenra.fr>
2022-05-03 18:06:39 +0200

    feat: Legal page

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,13 +1,3 @@
-// initialiser le mailto au moment du clic
-// document.body.querySelector("a[href=contact]").onclick = function(e) {
-//     var link = atob("bWFpbHRv")+":"+atob("Y29udGFjdEBsZW5yYS5pbw==");
-//     e.target.href = link;
-//     e.target.onclick = function(){};
-//     window.open(link);
-//     e.preventDefault();
-//     return false;
-// };
-
 // ajouter/générer le style sans les variables si elles ne sont pas gérées
 var computedStyle = getComputedStyle(document.body, null);
 if (computedStyle.backgroundColor=="transparent") {
