57bd0d09a4dbba0a0c936dbb1273d784dda873d6
Thomas DA ROCHA <8969556+taorepoara@users.noreply.github.com>
2022-09-16 18:27:48 +0200

    32 main page (#39)
    * start installation api twitter
    
    * home page header modification + animation
    
    * Header modification and animation almost done
    
    * homepage icon modification
    
    * responsive+js animation+manifesto
    
    * added usecases page
    
    * added team page
    
    * modification on team page
    
    * End of the dev of the carrousel + team page
    
    * carrousel finished, added social link
    
    * Team page finished
    
    * add Contact us page
    
    * modification on contact page
    
    * feat: add mails footer image (#37)
    
    * feat: Change doc link (#38)
    
    * fix: Index page
    
    * fix: Disable team and contact
    
    Co-authored-by: Thibault <thibault.cauche@lenra.io>
    Co-authored-by: Shiipou <shiishii@nocturlab.fr>
    Co-authored-by: Jonas Martinez <36544012+jonas-martinez@users.noreply.github.com>

---
+++ src/static/js/index.js
@@ -0,0 +1,27 @@
+setTimeout(closeLanguage, 1500);
+
+const languageList = document.querySelector("h1 > ul");
+let lastLanguagePosition = 0;
+const animationDuration = 1500;
+
+/**
+ * select randomly a language position and open it
+ */
+function changeLanguage() {
+    let position;
+    do {
+        position = Math.floor(Math.random() * languageList.children.length);
+    } while (position == lastLanguagePosition);
+    lastLanguagePosition = position;
+    languageList.style.setProperty('--language-position', position);
+    languageList.classList.remove("close");
+    setTimeout(closeLanguage, animationDuration);
+}
+
+/**
+ * close the selected language of the list
+ */
+function closeLanguage() {
+    languageList.classList.add("close");
+    setTimeout(changeLanguage, animationDuration);
+}
