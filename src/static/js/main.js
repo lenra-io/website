dbb0fd8e5f3b690a0361b8bab3fef6d6946d4ea3
Thomas DA ROCHA <thomas.darocha@lenra.io>
2023-03-14 17:24:05 +0100

    feat: Cookies explained

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,8 +1,19 @@
-// ajouter/générer le style sans les variables si elles ne sont pas gérées
-var computedStyle = getComputedStyle(document.body, null);
-if (computedStyle.backgroundColor=="transparent") {
-    console.log("Not managed variables");
-    var script = document.createElement("script");
-    script.src = "js/modernizer.js";
-    document.body.appendChild(script);
-}
+(() => {
+    const cookies = document.querySelector("body>#cookies");
+    const cookiesClass = "open";
+    const toggleBtn = cookies.querySelector(":scope>button");
+    const closeBtn = cookies.querySelector(":scope>section>button.lenra-icon-x");
+    const goodBtn = cookies.querySelector(":scope>section>nav>button.primary");
+    cookies.onclick = function (event) {
+        switch (event.target) {
+            case cookies:
+            case closeBtn:
+            case goodBtn:
+                cookies.classList.remove(cookiesClass);
+                break;
+            case toggleBtn:
+                cookies.classList.toggle(cookiesClass);
+                break;
+        }
+    }
+})();
