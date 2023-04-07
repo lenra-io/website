d881275df997250c82a0d0fef79219119ff2a9f9
Thomas DA ROCHA <thomas.darocha@lenra.io>
2023-04-07 16:11:17 +0200

    feat: Use airform.io

--- src/static/js/contact.js
+++ src/static/js/contact.js
@@ -3,10 +3,13 @@
     let form = document.querySelector("form");
     form.onsubmit = (e) => {
         const now = Date.now();
-        if (now - start > 3000) {
-            // TODO: send form
+        if (now - start < 3000) {
+            e.preventDefault();
+            return false;
         }
-        e.preventDefault();
-        return false;
+        // send form
+        form.action="https://airform.io/contact@lenra.io";
+        form.method="post";
+        form.target="_blank"
     };
 })();
