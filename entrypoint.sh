ced0146dad44f6bb27669b4a5c67468769d98f92
Thomas DA ROCHA <thomas.darocha@lenra.io>
2023-06-05 18:12:02 +0200

    feat: Replace Ghorgamel env var

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-envsubst -v '${GHORGAMEL_URL}' < /opt/bitnami/nginx/conf/server_blocks/nginx.conf.template > /opt/bitnami/nginx/conf/server_blocks/nginx.conf
+envsubst '${GHORGAMEL_URL}' < /opt/bitnami/nginx/conf/server_blocks/nginx.conf.template > /opt/bitnami/nginx/conf/server_blocks/nginx.conf
 
 . /opt/bitnami/scripts/nginx/entrypoint.sh "$@"
